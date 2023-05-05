b696004a7cebcdda07d1e7c425987e5a1b6f28f1
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-05-04 20:40:57 -0400

    update env database

--- .flaskenv
+++ .flaskenv
@@ -1,3 +1,5 @@
 FLASK_ENV=development
 FLASK_APP=main.py
 
+MONGODB=mongodb+srv://jaydunb12:Football123@cluster0.develxm.mongodb.net/
+
--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,22 @@
 
 from application import app
-from flask import render_template
+from flask import Response, render_template, request, json
+from flask_pymongo import pymongo
+
+from os import environ
+
+
+classData = [{"classID": "101", "title": "Hot Yoga", "instructor": "Kim", "duration": "60"},{"classID": "102", "title": "Group Yoga", "instructor": "Alex", "duration": "55"},
+                 {"classID": "103", "title": "Individual Yoga", "instructor": "Tonya", "duration": "70"},{"classID": "104", "title": "Outdoor Yoga", "instructor": "TBD", "duration": "65"},
+                 ]
+
+
+CONNECTION_STRING = environ.get('MONGODB')
+client = pymongo.MongoClient(CONNECTION_STRING)
+db = client.get_database('classes')
+user_collection = pymongo.collection.Collection(db, 'user_collection')
+
+
 
 @app.route("/")
 def index():
@@ -10,9 +26,7 @@ def index():
 
 @app.route("/courses")
 def courses():
-    classData = [{"classID": "101", "title": "Hot Yoga", "instructor": "Kim", "duration": "60"},{"classID": "102", "title": "Group Yoga", "instructor": "Alex", "duration": "55"},
-                 {"classID": "103", "title": "Individual Yoga", "instructor": "Tonya", "duration": "70"},{"classID": "104", "title": "Outdoor Yoga", "instructor": "TBD", "duration": "65"},
-                 ]
+
     return render_template("courses.html", classData = classData, courses=True)
 
 
@@ -30,3 +44,28 @@ def login():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/enrollment", methods=["GET", "POST"])
+def enrollment():
+    id = request.form.get('classID')
+    title = request.form.get('title')
+    duration = request.form.get('duration')
+    return render_template("enrollment.html", enrollment=True, data={"id": id, "title": title, "duration": duration})
+
+
+@app.route("/api/")
+@app.route("/api/<idx>")
+def api(idx=None):
+    if(idx == None):
+        jdata = classData 
+    else:
+        jdata = classData[int(idx)]
+    
+    return Response(json.dumps(jdata), mimetype="application/json")
+
+
+
+@app.route("/test")
+def test():
+    db.collection.insert_one({"classID": 107, "title": "In-Home Yoga", "instructor": "Tiffany", "duration": 75})
+    return "Added Successfully I Think"
